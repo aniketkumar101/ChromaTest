f559dc602590533bc220f4203a8133a3ff7dd1ce
Aniket Kumar <aniket787068@gmail.com>
2025-07-01 03:41:23 +0530

    user in backend

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const bodyParser = require('body-parser');
+const User = require('./route/Users');
+
+const dbconnect = require('./db/db');
+dbconnect()
+
+const app = express();
+app.use(cors());
+const port = 5000
+
+app.use(bodyParser.json());
+app.use('/users',User);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
